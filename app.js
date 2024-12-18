c24082db9d73f9135f8530f2a0ac1459d51b0a76
Salla <salla_mikkonen@hotmail.com>
2024-12-18 15:41:42 +0200

    feat: implement fully functional RESTful API with validation and error handling

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const app = express();
+const {
+  body,
+  query,
+  matchedData,
+  validationResult,
+} = require("express-validator");
 const port = process.env.PORT || 3000;
 const path = require("path");
 
-let counter = 3;
+let counter = 2;
 
 let db = [
-  { id: 1, name: "jack" }, // 0
-  { id: 2, name: "tina" }, // 1
+  { id: 1, latitude: 40.7128, longitude: -74.006 },
+  { id: 2, latitude: 32.7828, longitude: -118.006 },
 ];
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/customers", (req, res) => {
+app.get("/api/locations", (req, res) => {
   res.send(db);
 });
 
-app.get("/customers/:urlId([0-9]+)", (req, res) => {
+app.get("/api/locations/:urlId([0-9]+)", (req, res) => {
   const id = Number(req.params.urlId); //
   const temp = db.find((customer) => customer.id === id);
   if (temp) {
@@ -27,16 +33,38 @@ app.get("/customers/:urlId([0-9]+)", (req, res) => {
   }
 });
 
-app.post("/customers", (req, res) => {
-  const customer = req.body;
-  customer.id = counter++;
-  db.push(customer);
-  res.send(customer);
-});
+app.post(
+  "/api/locations",
+  [
+    body("latitude")
+      .isFloat({ min: -90, max: 90 })
+      .withMessage("Invalid latitude")
+      .toFloat(),
+    body("longitude")
+      .isFloat({ min: -180, max: 180 })
+      .withMessage("Invalid longitude")
+      .toFloat(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-app.delete("/customers/:urlId([0-9]+)", (req, res) => {
+    const data = matchedData(req);
+    const location = {
+      ...data,
+      id: counter++,
+    };
+
+    db.push(location);
+    res.json(location);
+  },
+);
+
+app.delete("/api/locations/:urlId([0-9]+)", (req, res) => {
   const id = Number(req.params.urlId); //
-  db = db.filter((customer) => customer.id != id);
+  db = db.filter((location) => location.id != id);
   res.status(204).end();
 });
 
